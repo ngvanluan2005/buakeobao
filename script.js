8b493d7680d5eb9a32fa01cca9797208b2f2cce4
Luan Nguyen <vanluandvlp@gmail.com>
2022-05-07 10:45:12 +0700

    bkb

---
+++ script.js
@@ -0,0 +1,54 @@
+
+const luachon = document.querySelector('.luachon')
+const gridBtn = [...document.querySelectorAll('.dapan')]
+gridBtn.map(btn => {
+    btn.addEventListener('click', event => {
+        const nguoichoira = event.target.dataset.type
+        event.target.classList.add('clicked')
+        // console.log(luonWin(nguoichoira));
+        luachon.innerHTML = `
+        <span style="font-size: 1.5rem">Ban da chon !</span>
+        ${aiTurn(nguoichoira)}
+        -----
+        `
+        setTimeout(() => {
+            luachon.innerHTML = `
+                ${aiTurn(nguoichoira)}
+                <div class="tbao">
+                    <span style="font-size: 1.5rem; padding: 4px 0;">Ban thua !</span>
+                    <button onclick="location.reload()">Choi Lai</button>
+                </div>
+                ${luonWin(nguoichoira)}
+            `
+            localStorage.setItem('ai', parseInt(localStorage.getItem('ai')) + 1)
+            updatePoint()
+        }, 2000)
+    })
+})
+const aiTurn = (turn) => {
+    return `
+        <img
+            data-type="${turn}"
+            class="dapan ${turn}"
+            src="./images/${turn}.png"
+            alt="${turn}">
+    `
+}
+
+const luonWin = peo => {
+    const logic = peo == 'bua' ? 'bao'
+    : peo == 'keo' ? 'bua'
+    : peo == 'bao' ? 'keo' : 1
+    return aiTurn(logic)
+}
+window.addEventListener('load', () => {
+    if (localStorage.getItem("ai") === null && localStorage.getItem("peo") === null) {
+        localStorage.setItem('ai', 0)
+        localStorage.setItem('peo', 0)
+    }
+    updatePoint()
+})
+const updatePoint = () => {
+    document.getElementById('points-may').innerHTML = localStorage.getItem('ai')
+    document.getElementById('points-nguoi').innerHTML = localStorage.getItem('peo')
+}
